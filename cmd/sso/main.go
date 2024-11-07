da20cc0c0529f0c4e0284d92b57220b48dfb4879
arishastugi <arisha.stugi@gmail.com>
2024-11-07 22:55:41 +0300

    init project + init config

---
+++ cmd/sso/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"sso/internal/config"
+)
+
+func main() {
+	// TODO: инициализация объекта конфига
+	cfg := config.MustLoad()
+
+	// TODO: инициализация объекта логгера
+
+	// TODO: инициализация объекта приложения (app)
+
+	// TODO: запуск приложения gRPC-сервер приложения (app.Run())
+}
